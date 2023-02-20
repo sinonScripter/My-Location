271cc7a8c2653404739ca3b0ec7b002111b7dab8
Leonardo Moreno <leonardo_dwg@hotmail.com>
2023-02-20 13:05:09 -0300

    my location

--- App.tsx
+++ App.tsx
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+import { View } from 'react-native';
+import MapView, { Marker }  from 'react-native-maps';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy
+  
+} from 'expo-location';
+
+import { styles } from './styles';
+
+
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null >(null);
+
+const mapRef = useRef<MapView>(null);
+
+
+  async function requestLocationPermissions() {
+    const { granted} = await  requestForegroundPermissionsAsync();
+
+    if(granted){
+     const currentPosition  = await getCurrentPositionAsync ();
+     setLocation(currentPosition);
+     
+    }
+    
+  }
+
+useEffect(() => {
+   requestLocationPermissions();
+}, []);
+
+useEffect(() =>{
+  watchPositionAsync({
+    accuracy: LocationAccuracy.Highest,
+    timeInterval: 1000,
+    distanceInterval: 1 
+  }, (response) =>{
+    setLocation(response);
+    mapRef.current?.animateCamera({
+      pitch: 70,
+      center: response.coords
+    })
+  });
+}, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+{
+  location &&
+   <MapView 
+   ref={mapRef}
+    style={styles.map}
+    initialRegion={{
+      latitude:location.coords.latitude,
+      longitude:location.coords.longitude,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005
+    }}
+    
+  >
+    <Marker 
+     coordinate={{
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+     }}
+    />
+  </MapView>
+  }
+
+
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
